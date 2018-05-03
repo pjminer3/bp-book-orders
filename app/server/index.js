ad2586525f5db70606fa8bb482a9bc8baf7210d7
Patrick Miner <patrickminer318@gmail.com>
2018-05-03 00:12:35 -0700

    Fixed process.env.PORT bug

--- app/server/index.js
+++ app/server/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
   console.log('listening on port 3000!');
